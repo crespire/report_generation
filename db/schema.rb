1ccce931ead3b8edf41f0d49bc36e6bc33ff1501
crespire <li.simmon@gmail.com>
2023-01-04 00:20:57 -0500

    feat: drop reports table from schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_01_03_200635) do
+ActiveRecord::Schema[7.0].define(version: 2023_01_04_051730) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -24,15 +24,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_03_200635) do
   create_table "projects", force: :cascade do |t|
     t.string "name"
     t.decimal "budget"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "reports", force: :cascade do |t|
-    t.string "client"
-    t.string "type"
-    t.date "start_date"
-    t.date "end_date"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
