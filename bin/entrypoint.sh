4fbf741e1cf1d1e82e8200947288835b3112299a
crespire <li.simmon@gmail.com>
2023-01-05 08:34:49 -0500

    fix: entrypoint sets up database in rails image

--- bin/entrypoint.sh
+++ bin/entrypoint.sh
@@ -2,7 +2,12 @@
 set -e
 
 # Remove a potentially pre-existing server.pid for Rails.
-rm -f /myapp/tmp/pids/server.pid
+rm -f ./tmp/pids/server.pid
 
-# Then exec the container's main process (what's set as CMD in the Dockerfile).
+# Make sure db is ready to go
+# Adding '2>/dev/null' sends output to nowhere in the case of an error and the
+# error code also triggers the bash OR to run db:setup
+bundle exec rails db:migrate 2>/dev/null || bundle exec rails db:setup
+
+# Then exec the container's main process (CMD in the Dockerfile).
 exec "$@"
